50eb11f044591903e6d41f98e04347a78d5ba2c0
Piotr Czapski <pczapski@users.noreply.github.com>
2018-06-17 23:43:59 +0200

    - Product list component - Product interface - Custom pipe - OnInit hook - Property binding - Event binding - Two-way binding

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './products/product-list.component';
+import {FormsModule} from "@angular/forms";
+import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductListComponent,
+        ConvertToSpacesPipe
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
