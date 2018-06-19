9c020df226d2f9b497f417cf69a7df036a0ef5fc
Piotr Czapski <pczapski@users.noreply.github.com>
2018-06-19 21:07:14 +0200

    - Product service

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import {Component} from "@angular/core";
+import {ProductService} from "./products/product.service";
 
 @Component({
     selector: 'pm-root',
     template: `
-    <div>
-    <h1>{{pageTitle}}</h1>
-        <pm-products></pm-products>
-    </div>
-    `
+        <div>
+            <h1>{{pageTitle}}</h1>
+            <pm-products></pm-products>
+        </div>
+    `,
+    providers: [ProductService]
 })
 export class AppComponent {
-  pageTitle: string = 'Acme Product Management';
+    pageTitle: string = 'Acme Product Management';
 }
